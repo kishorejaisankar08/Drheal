0b0b01563863c2d8d4f5838a1df9d5df1a0e0e6a
kishorejaisankar08 <82252257+kishorejaisankar08@users.noreply.github.com>
2021-04-10 11:43:11 +0530

    added remote mysql connection details

--- classes/Config.class.php
+++ classes/Config.class.php
@@ -5,10 +5,10 @@ class Config{
 
 	// database
 
-	const DB_HOST = "localhost"; // database host or server
-	const DB_NAME = "hospital"; // the actual name of the system's database [database name]
-	const DB_USER = "root"; // the actual name of database user
-	const DB_PASSWORD = ""; // database password
+	const DB_HOST = "remotemysql.com"; // database host or server
+	const DB_NAME = "id8kxkVJsh"; // the actual name of the system's database [database name]
+	const DB_USER = "id8kxkVJsh"; // the actual name of database user
+	const DB_PASSWORD = "xIh7ZUmmro"; // database password
 
 
 	// SYSTEM 
